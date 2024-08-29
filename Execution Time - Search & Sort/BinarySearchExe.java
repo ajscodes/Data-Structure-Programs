93cc81a76f84a8a26e63b13dc202396876c4b75d
ajscodes <ayushmaradia@gmail.com>
2024-08-29 21:14:30 +0530

    Exe timing for Binary Search

---
+++ Execution Time - Search & Sort/BinarySearchExe.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class BinarySearchExe {
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        Scanner op = new Scanner(System.in);
+
+        System.out.print("Enter size of an array: ");
+        int size = op.nextInt();
+        
+        int[] n = new int[size];
+        
+        System.out.println("Enter elements of array: ");
+        for (int j = 0; j < size; j++) {
+            System.out.print("n[" + j + "] = ");
+            n[j] = op.nextInt();
+        }
+
+        System.out.print("Enter the element to be searched: ");
+        int e = op.nextInt();
+
+        int left = 0;
+        int right = n.length - 1;
+        Arrays.sort(n);
+        boolean found = false;
+        
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            System.out.println("Middle element is at index " + middle);
+            if (n[middle] == e) {
+                System.out.println(e + " is found at index " + middle + ".");
+                found = true;
+                break; 
+            } else if (n[middle] < e) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        if (!found) {
+            System.out.println("404 Not Found.");
+        }
+
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        System.out.println("Execution time in milliseconds: " + executionTime / 1_000_000 + " ms");
+    }
+}
